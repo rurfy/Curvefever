1bfbde011c60ef6744cfaa38bce7bb95da99c93c
LucasMiekley <LucasMiekley@BK7N00105399.bku.db.de>
2018-03-27 13:40:12 +0200

    Bewegung nach links eingefügt und Bug, dass sich der Kreis etwas nach oben bewegt

--- Curvefever/Var.java
+++ Curvefever/Var.java
@@ -1,6 +1,8 @@
 
 public class Var {
-	static int player1x = 100, player1y = 100;
+	static int player1x = 500, player1y = 500;
 	static int width, height;
-	static boolean links1=false, rechts1=false;
+	static boolean links1 = false, rechts1 = false;
+	static int hoch = 0, runter = 0, links = -5, rechts = 0;
+	static int hochMax = -5, runterMax = 5, linksMax = -5, rechtsMax = 5;
 }
